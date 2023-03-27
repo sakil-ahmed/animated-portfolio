5713cf3baf79381607fc5573d465bfd9e246d82b
sakil-ahmed <sakilahmed1495@gmail.com>
2023-03-28 03:46:59 +0600

    update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,10 +3,33 @@ import "./Card.scss";
 import { HiEye } from "react-icons/hi";
 import { AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Card = ({ item: { img, subTitle, path, git } }) => {
+  const [direction, setDirection] = useState("bottom");
+
+  const handleMouseEnter = (event) => {
+    const button = event.target;
+    const rect = button.getBoundingClientRect();
+    const x = event.clientX - rect.x;
+    const y = event.clientY - rect.y;
+
+    if (x <= rect.width / 3) {
+      setDirection("left");
+    } else if (x >= (rect.width * 2) / 3) {
+      setDirection("right");
+    } else if (y <= rect.height / 3) {
+      setDirection("top");
+    } else if (y >= (rect.height * 2) / 3) {
+      setDirection("bottom");
+    } else {
+      setDirection("bottom");
+    }
+  };
+
   return (
     <motion.div
+      onMouseEnter={handleMouseEnter}
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true, amount: 0.5 }}
@@ -20,7 +43,7 @@ const Card = ({ item: { img, subTitle, path, git } }) => {
       <div className="card__thumbnail">
         <img src={img} alt="Project Thumbnail" />
       </div>
-      <div className="card__content">
+      <div className={`card__content ${direction}`}>
         <h3>Website Development</h3>
         <p>{subTitle}</p>
         <div className="icons">
