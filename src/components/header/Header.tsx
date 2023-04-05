518feb4774f604e5625498d2b6e896961f5abef9
sakil-ahmed <sakilahmed1495@gmail.com>
2023-04-06 03:35:50 +0600

    update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import "./Header.scss";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
+import { TbArrowBackUp } from "react-icons/tb";
 import { MdClose } from "react-icons/md";
 import Menubar from "./navbar/Navbar";
 import logo from "./../../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { pathname } = useLocation();
 
   return (
     <header className="header" id="header">
@@ -35,7 +37,13 @@ const Header = () => {
                 <MdClose />
               </button>
             </div>
-            <Menubar setIsMenuOpen={setIsMenuOpen} />
+            {pathname === "/work" ? (
+              <Link to={"/"} style={{ color: "#000000" }}>
+                <TbArrowBackUp size={"35px"} />
+              </Link>
+            ) : (
+              <Menubar setIsMenuOpen={setIsMenuOpen} />
+            )}
           </nav>
           <div className="header__btn">
             <a className="resume__btn" href="Resume.pdf" download>
